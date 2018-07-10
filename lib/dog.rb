bddbd256e7466ac45fc8ce00661b7daa3357cf7f
Hayden Betts <haydenbetts@gmail.com>
2018-07-10 21:18:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,8 +12,18 @@ class Dog
     # drops dogs
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
     # creates and returns new dog instance from database row
+  end
+
+  def self.find_by_name(name)
+    # returns dog instance with that name
+  end
+
+  def update
+  end
+
+  def save
   end
 
 end
